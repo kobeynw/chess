03afb5ae0742661b6fc7527bf3e1ec8006214a27
Kobey Workman <kobeyworkman1@gmail.com>
2024-09-13 19:19:10 -0600

    Create BishopMoves as subclass of MoveCalculator
    Used to calculate the possible moves of the Bishop chess piece. Returns a collection of chess moves.

---
+++ shared/src/main/java/chess/BishopMoves.java
@@ -0,0 +1,79 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class BishopMoves extends MoveCalculator {
+    private final Collection<ChessMove> moves = new ArrayList<>();
+
+    public BishopMoves(ChessPiece piece, ChessBoard board, ChessPosition position) {
+        super(piece, board, position);
+    }
+
+    public Collection<ChessMove> getBishopMoves() {
+        int row = position.getRow();
+        int col = position.getColumn();
+        int nextRow = row;
+        int nextCol = col;
+
+        while (nextRow <= 8 && nextCol <= 8) {
+            nextRow++;
+            nextCol++;
+
+            boolean endCheck = checkPosition(nextRow, nextCol);
+            if (endCheck) {
+                nextRow = row;
+                nextCol = col;
+                break;
+            }
+        }
+
+        while (nextRow >= 1 && nextCol <= 8) {
+            nextRow--;
+            nextCol++;
+
+            boolean endCheck = checkPosition(nextRow, nextCol);
+            if (endCheck) {
+                nextRow = row;
+                nextCol = col;
+                break;
+            }
+        }
+
+        while (nextRow >= 1 && nextCol >= 1) {
+            nextRow--;
+            nextCol--;
+
+            boolean endCheck = checkPosition(nextRow, nextCol);
+            if (endCheck) {
+                nextRow = row;
+                nextCol = col;
+                break;
+            }
+        }
+
+        while (nextRow <= 8 && nextCol >= 1) {
+            nextRow++;
+            nextCol--;
+
+            boolean endCheck = checkPosition(nextRow, nextCol);
+            if (endCheck) {
+                break;
+            }
+        }
+
+        return moves;
+    }
+
+    private boolean checkPosition(int nextRow, int nextCol) {
+        ChessPosition nextPosition = new ChessPosition(nextRow, nextCol);
+        ChessMove nextMove = new ChessMove(position, nextPosition, null);
+
+        if (!moveBlocked(nextPosition)) {
+            moves.add(nextMove);
+            return canTakePiece(nextPosition);
+        } else {
+            return true;
+        }
+    }
+}
