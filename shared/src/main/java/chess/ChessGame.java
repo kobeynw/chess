8934c525d1ab4b4ab0614d2ec16323fb95861640
Kobey Workman <kobeyworkman1@gmail.com>
2024-09-30 13:14:47 -0600

    Update ChessGame with board get and set methods
    Added a class variable board, and updated setBoard and getBoard using this variable.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -9,6 +9,7 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
+    private ChessBoard board = new ChessBoard();
 
     public ChessGame() {
 
@@ -96,7 +97,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -105,6 +106,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return this.board;
     }
 }
