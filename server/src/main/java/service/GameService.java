7fd0d8e9bbb7624de96751b28a47ef600ce18811
Kobey Workman <kobeyworkman1@gmail.com>
2024-10-17 17:07:46 -0600

    Create service classes and create service tests
    Created classes to handle game services and user services, instead of using separate service classes for each. Created boilerplate test classes for each service class.

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,5 @@
+package service;
+
+public class GameService {
+    // NOTE: Implements create game, join game, and list games services
+}
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,5 @@
+package service;
+
+public class UserService {
+    // NOTE: Implements login, logout, and register services
+}
