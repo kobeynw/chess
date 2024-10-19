7e26f2f036c5906f7e298f7f94ab1c4bc61784bd
Kobey Workman <kobeyworkman1@gmail.com>
2024-10-18 23:33:27 -0600

    Update UserServiceTest to test login service
    Updated UserServiceTest with methods to start server before all tests, stop server after all tests, and test loginService method.

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,5 +1,46 @@
 package service;
 
+import dataaccess.DataAccessException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import request.LoginRequest;
+import result.LoginResult;
+import server.Server;
+
 public class UserServiceTest {
-    // NOTE: Utilize the before and after method decorators to start and stop the server
+    private static final Server server = new Server();
+
+    @BeforeAll
+    public static void startServer() {
+        server.run(8080);
+    }
+
+    @Test
+    public void testLoginService() {
+        String usernameExpected = "username";
+        String password = "password";
+
+        LoginRequest loginRequest = new LoginRequest(usernameExpected, password);
+        UserService userService = new UserService();
+
+        try {
+            LoginResult loginResult = userService.loginService(loginRequest);
+
+            String usernameOutput = loginResult.username();
+            String authTokenOutput = loginResult.authToken();
+
+            Assertions.assertEquals(usernameOutput, usernameExpected);
+            Assertions.assertNotNull(authTokenOutput);
+        } catch (DataAccessException e) {
+            System.out.printf("{ \"message\": \"Error: %s\" }%n", e.getMessage());
+        }
+    }
+
+    @AfterAll
+    public static void stopServer() {
+        server.stop();
+    }
 }
