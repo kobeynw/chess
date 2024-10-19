2a2b2f1ca190101ad5d7d9d1d69d68a45b317e88
Kobey Workman <kobeyworkman1@gmail.com>
2024-10-18 23:07:21 -0600

    Update login service class
    Updated login service class for end to end login implementation.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,6 +1,42 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import model.AuthData;
+import model.UserData;
+import request.LoginRequest;
+import result.LoginResult;
+
 public class UserService extends Services {
     // NOTE: Implements login, logout, and register services
     // NOTE: Utilize the Services parent class to validate auth tokens
+
+    public UserService() {}
+
+    public LoginResult loginService(LoginRequest loginRequest) throws DataAccessException {
+        UserData userData = validateUser(loginRequest);
+
+        MemoryAuthDAO authDao = new MemoryAuthDAO();
+        AuthData authData = authDao.createAuth(userData);
+
+        String username = authData.username();
+        String authToken = authData.authToken();
+
+        return new LoginResult(username, authToken);
+    }
+
+    private UserData validateUser(LoginRequest loginRequest) throws DataAccessException {
+        String username = loginRequest.username();
+        String password = loginRequest.password();
+
+        MemoryUserDAO userDao = new MemoryUserDAO();
+        UserData userData = userDao.getUser(username, password);
+
+        if (userData == null) {
+            throw new DataAccessException("Invalid User");
+        } else {
+            return userData;
+        }
+    }
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,4 +1,5 @@
 package service;
 
 public class UserServiceTest {
+    // NOTE: Utilize the before and after method decorators to start and stop the server
 }
