09de4ddc42d45c751cd6c6456fc651e2d6507f17
Kobey Workman <kobeyworkman1@gmail.com>
2024-10-18 23:06:31 -0600

    Update login handler class
    Updated abstract handler class and login handler class for end to end login implementation.

--- server/src/main/java/handler/Handlers.java
+++ server/src/main/java/handler/Handlers.java
@@ -1,8 +1,26 @@
 package handler;
 
-public class Handlers {
+import com.google.gson.JsonObject;
+import spark.*;
+import com.google.gson.Gson;
+
+import java.util.Objects;
+
+public abstract class Handlers {
     // TODO: Create methods to serialize and deserialize data (to be used in each sub-class)
     // NOTE: Make sure that the field names used in GSON are exactly the same as those in the Request and Result classes
     // NOTE: Utilize the fact that GSON does not serialize null fields in order to return an error message instead of
     //       the normal result fields
+
+    public JsonObject serialize(Request req, String type) {
+        Gson gson = new Gson();
+
+        if (Objects.equals(type, "body")) {
+            String body = req.body();
+            return gson.fromJson(body, JsonObject.class);
+        } else {
+            String header = req.headers("authToken");
+            return gson.fromJson(header, JsonObject.class);
+        }
+    }
 }
--- server/src/main/java/handler/LoginHandler.java
+++ server/src/main/java/handler/LoginHandler.java
@@ -1,5 +1,17 @@
 package handler;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dataaccess.DataAccessException;
+import request.LoginRequest;
+import result.LoginResult;
+import service.UserService;
+import spark.*;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+
 public class LoginHandler extends Handlers {
     // Basic Outline:
     // Deserialize JSON request body to Java request object
@@ -9,4 +21,28 @@ public class LoginHandler extends Handlers {
     // Send HTTP response back to client with appropriate status code and response body
 
     // NOTE: utilize the serialization/deserialization from the Handlers parent class
+
+    public LoginHandler() {}
+
+    public Object handleRequest(Request req, Response res) {
+        try {
+            JsonObject bodyJsonObject = serialize(req, "body");
+
+            String username = bodyJsonObject.get("username").getAsString();
+            String password = bodyJsonObject.get("password").getAsString();
+            LoginRequest loginRequest = new LoginRequest(username, password);
+
+            UserService userService = new UserService();
+            LoginResult loginResult = userService.loginService(loginRequest);
+
+            String resultBody = new Gson().toJson(loginResult);
+            res.status(200);
+
+            return resultBody;
+        } catch (DataAccessException e) {
+            res.status(500);
+
+            return String.format("{ \"message\": \"Error: %s\" }", e.getMessage());
+        }
+    }
 }
