c23f3fdfe6bd1430a09bf183eee990ce8df3d1c1
Kobey Workman <kobeyworkman1@gmail.com>
2024-11-23 18:41:18 -0700

    Add ServerMessage subclasses and UserGameCommand subclasses
    Created subclasses for ServerMessage: ErrorMessage, LoadGameMessage, and NotificationMessage. Created subclass MakeMoveCommand for UserGameCommand parent class.

---
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -0,0 +1,10 @@
+package websocket.messages;
+
+public class ErrorMessage extends ServerMessage {
+    String message;
+
+    public ErrorMessage(ServerMessageType type, String message) {
+        super(type);
+        this.message = message;
+    }
+}
