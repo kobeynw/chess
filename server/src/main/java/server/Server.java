b968366653c2bafcb0c377dabbb73490e37d8ea9
Kobey Workman <kobeyworkman1@gmail.com>
2024-12-03 15:42:13 -0700

    Update Server with webSocket endpoint. Update WebsocketCommunicator with adapter import
    Updated Server with webSocket endpoint, to make sure it is the first endpoint registered. Updated WebsocketCommunicator to include the Server Message type adapter import.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,6 +3,7 @@ package server;
 import dataaccess.*;
 import spark.*;
 import handler.*;
+import websocket.WebSocketHandler;
 
 public class Server {
     UserDAO userDao;
@@ -38,6 +39,8 @@ public class Server {
     }
 
     private void registerEndpoints() {
+        Spark.webSocket("/ws", new WebSocketHandler(authDao, gameDao));
+
         Spark.post("/user", (req, res) -> (new RegisterHandler(userDao, authDao)).handleRequest(req, res));
         Spark.post("/session", (req, res) -> (new LoginHandler(userDao, authDao)).handleRequest(req, res));
         Spark.delete("/session", (req, res) -> (new LogoutHandler(userDao, authDao)).handleRequest(req, res));
@@ -45,8 +48,6 @@ public class Server {
         Spark.post("/game", (req, res) -> (new CreateGameHandler(gameDao, authDao)).handleRequest(req, res));
         Spark.put("/game", (req, res) -> (new JoinGameHandler(gameDao, authDao)).handleRequest(req, res));
         Spark.get("/game", (req, res) -> (new ListGamesHandler(gameDao, authDao)).handleRequest(req, res));
-
-        Spark.webSocket("/ws", new WebSocketHandler(authDao));
 
         Spark.delete("/db", (req, res) -> (new ClearApplicationHandler(userDao, authDao, gameDao)).handleRequest(req, res));
     }
