0017dbdb13f196126754f510369f3ba5e4755fb7
Kobey Workman <kobeyworkman1@gmail.com>
2024-12-03 15:42:27 -0700

    Update Server with webSocket endpoint. Update WebsocketCommunicator with adapter import
    Updated Server with webSocket endpoint, to make sure it is the first endpoint registered. Updated WebsocketCommunicator to include the Server Message type adapter import.

--- client/src/main/java/network/WebsocketCommunicator.java
+++ client/src/main/java/network/WebsocketCommunicator.java
@@ -1,23 +1,17 @@
 package network;
 
-import chess.ChessGame;
 import chess.ChessMove;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
+
 import ui.ServerMessageObserver;
+import websocket.adapters.ServerMessageAdapter;
 import websocket.commands.*;
 import websocket.messages.ErrorMessage;
-import websocket.messages.LoadGameMessage;
-import websocket.messages.NotificationMessage;
 import websocket.messages.ServerMessage;
 
 import javax.websocket.*;
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 public class WebsocketCommunicator extends Endpoint {
     public Session session;
@@ -29,7 +23,6 @@ public class WebsocketCommunicator extends Endpoint {
             this.session = container.connectToServer(this, uri);
 
             this.session.addMessageHandler(new MessageHandler.Whole<String>() {
-                @Override
                 public void onMessage(String message) {
                     GsonBuilder builder = new GsonBuilder();
                     builder.registerTypeAdapter(ServerMessage.class, new ServerMessageAdapter());
@@ -39,7 +32,7 @@ public class WebsocketCommunicator extends Endpoint {
                     observer.notify(serverMessage);
                 }
             });
-        } catch (DeploymentException | IOException | URISyntaxException ex) {
+        } catch (Exception ex) {
             ErrorMessage errorMessage = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, ex.getMessage());
             observer.notify(errorMessage);
         }
@@ -66,49 +59,4 @@ public class WebsocketCommunicator extends Endpoint {
     }
 
     public void onOpen(Session session, EndpointConfig endpointConfig) {}
-
-    private static class ServerMessageAdapter extends TypeAdapter<ServerMessage> {
-        @Override
-        public void write(JsonWriter jsonWriter, ServerMessage serverMessage) throws IOException {
-            Gson gson = new Gson();
-
-            switch(serverMessage.getServerMessageType()) {
-                case LOAD_GAME -> gson.getAdapter(LoadGameMessage.class).write(jsonWriter, (LoadGameMessage) serverMessage);
-                case ERROR -> gson.getAdapter(ErrorMessage.class).write(jsonWriter, (ErrorMessage) serverMessage);
-                case NOTIFICATION -> gson.getAdapter(NotificationMessage.class).write(jsonWriter, (NotificationMessage) serverMessage);
-            }
-        }
-
-        @Override
-        public ServerMessage read(JsonReader jsonReader) throws IOException {
-            ServerMessage.ServerMessageType serverMessageType = null;
-            String errorMessage = null;
-            ChessGame game = null;
-            String message = null;
-
-            jsonReader.beginObject();
-
-            while (jsonReader.hasNext()) {
-                String name = jsonReader.nextName();
-                switch (name) {
-                    case "serverMessageType" -> serverMessageType = ServerMessage.ServerMessageType.valueOf(jsonReader.nextString());
-                    case "errorMessage" -> errorMessage = jsonReader.nextString();
-                    case "game" -> game = new Gson().fromJson(jsonReader, ChessGame.class);
-                    case "message" -> message = jsonReader.nextString();
-                }
-            }
-
-            jsonReader.endObject();
-
-            if(serverMessageType == null) {
-                return null;
-            } else {
-                return switch (serverMessageType) {
-                    case LOAD_GAME -> new LoadGameMessage(serverMessageType, game);
-                    case ERROR -> new ErrorMessage(serverMessageType, errorMessage);
-                    case NOTIFICATION -> new NotificationMessage(serverMessageType, message);
-                };
-            }
-        }
-    }
 }
